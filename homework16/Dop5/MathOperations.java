5327ab19599eaf79649a2c6d71bd0338bdac3fad
Ivanna <kasjawiza@mail.ru>
2019-02-26 00:28:40 +0300

    hometask16

---
+++ homework16/Dop5/MathOperations.java
@@ -0,0 +1,29 @@
+package core;
+
+public class MathOperations {
+
+	int a = 10;
+	int b = 0;
+	
+	
+
+	public int sumElement(int a, int b) {
+		return (a + b);
+	}
+
+	public int deductionElement(int a, int b) {
+		return (a - b);
+	}
+
+	public int multiplyElement(int a, int b) {
+		return (a * b);
+	}
+
+	public double divisionElement(int a, int b) {
+		return (a/b);
+	}
+
+	public int squareElement(int a) {
+		return (a * a);
+	}
+}
